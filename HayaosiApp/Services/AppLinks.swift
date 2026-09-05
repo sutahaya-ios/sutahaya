24d88e13b6f6e5bca1fdeb142a248117092ef574
TOKIYA-YAMAMOTO <tokiya.yamamoto0616@gmail.com>
2026-09-06 04:36:15 +0900

    chore: App Store URLを設定

--- HayaosiApp/Services/AppLinks.swift
+++ HayaosiApp/Services/AppLinks.swift
@@ -2,6 +2,6 @@ import Foundation
 
 /// アプリ外へ案内するURLを、申請後に1か所だけ更新できるよう管理する
 enum AppLinks {
-    /// App ID確定後にApp Store URLを設定する。未申請中は共有文面へ無効なURLを載せないためnilにする
-    static let appStoreURL: URL? = nil
+    /// App Storeの商品ページ。ルーム招待の共有文面へ掲載する
+    static let appStoreURL = URL(string: "https://apps.apple.com/jp/app/id6800718737")
 }
